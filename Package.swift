c915056a02bb9522d270aa1b237d8cac9d88846e
Henrich Mauritz <henrich.mauritz@icloud.com>
2023-06-21 19:05:24 +0200

    set to dynamic library

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "SygicUbiKit",
+            type: .dynamic,
             targets: ["SygicUbiKit"]),
     ],
     dependencies: [
