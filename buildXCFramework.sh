3ac90dc4ee8b6a76c004dfd5034cd0f30bf0b9df
Henrich Mauritz <henrich.mauritz@icloud.com>
2023-06-26 20:01:31 +0200

    script added

---
+++ buildXCFramework.sh
@@ -0,0 +1,20 @@
+xcodebuild archive \
+            -project SygicUbiKit.xcodeproj \
+            -scheme SygicUbiKit \
+            -destination "generic/platform=iOS Simulator" \
+            -archivePath "archives/MyScheme-iOS-sim" \
+            -allowProvisioningUpdates \
+            SKIP_INSTALL=NO \
+            BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+
+xcodebuild archive \
+            -project SygicUbiKit.xcodeproj \
+            -scheme SygicUbiKit \
+            -destination "generic/platform=iOS" \
+            -archivePath "archives/MyScheme-iOS" \
+            -allowProvisioningUpdates \
+            SKIP_INSTALL=NO \
+            BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework -framework "/Users/henrichmauritz/Documents/Sygic/Packages/SygicUbiKit/archives/MyScheme-iOS.xcarchive/Products/Library/Frameworks/SygicUbiKit.framework" -debug-symbols "/Users/henrichmauritz/Documents/Sygic/Packages/SygicUbiKit/archives/MyScheme-iOS.xcarchive/dSYMs/SygicUbiKit.framework.dSYM" -framework "/Users/henrichmauritz/Documents/Sygic/Packages/SygicUbiKit/archives/MyScheme-iOS-sim.xcarchive/Products/Library/Frameworks/SygicUbiKit.framework" -debug-symbols "/Users/henrichmauritz/Documents/Sygic/Packages/SygicUbiKit/archives/MyScheme-iOS-sim.xcarchive/dSYMs/SygicUbiKit.framework.dSYM" -output "/Users/henrichmauritz/Documents/Sygic/Packages/Output/SygicUbiKit.xcframework"
