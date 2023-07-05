a10b906ad57e578afe5679b560272011e75b10a9
Juraj Antas <juraj.antas@gmail.com>
2023-07-05 09:05:14 +0200

    4.1.16

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SygicUbiKit",
-            url: "https://github.com/henrich-mauritz/SygicUbiKit/releases/download/4.1.15/SygicUbiKit.xcframework-4.1.15.zip",
+            url: "https://github.com/henrich-mauritz/SygicUbiKit/releases/download/4.1.15/SygicUbiKit.xcframework-4.1.16.zip",
             checksum: "9654aab914f8a62ac8f26373b99c3c6e04e86d97b231e1530d4390869c590ac5")
     ]
 )
