101ae846f86452745bf33fbfca44cfc003adbdb1
runner <runner@Mac-1689934951184.local>
2023-07-21 10:34:45 +0000

    updating package to 4.2.3

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SygicUbiKit",
-            url: "https://github.com/henrich-mauritz/SygicUbiKit/releases/download/4.2.2/SygicUbiKit.xcframework-4.2.2.zip",
-            checksum: "a1ac475eec877a5cc7a82e61694a276a99f61b88eda748e4d60530ed344b7497")
+            url: "https://github.com/henrich-mauritz/SygicUbiKit/releases/download/4.2.3/SygicUbiKit.xcframework-4.2.3.zip",
+            checksum: "5e66e2fcd1bf5e9570c9ea00bc1d38898258bc8ab255458fa58505274ef97c91")
     ]
 )
